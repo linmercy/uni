09d63af939bcaf05f6db56309de82a2d1811bd7a
mercylin muthoni <linmercymuthoni@gmail.com>
2023-05-09 23:55:55 +0300

    home page

---
+++ index.js
@@ -0,0 +1,10 @@
+// Toggle Menu
+var navLink = document.getElementById("navLink");
+
+function showMenu(){
+    navLink.style.right = '0';
+}
+
+function hideMenu(){
+    navLink.style.right = '-200px';
+}
